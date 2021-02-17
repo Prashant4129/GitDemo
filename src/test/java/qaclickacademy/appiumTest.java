9ae96dd74bbcddc5bb794948cc1605e0e2039aa6
Prashant <prashantmarne70@gmail.com>
2021-02-17 15:15:30 +0530

    first commit

---
+++ src/test/java/qaclickacademy/appiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class appiumTest {
+	@Test
+	public void NativeApp()
+	{
+		System.out.println("NativeApp");
+	}
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/seleniumjavaTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class seleniumjavaTest {
+	@Test
+	public void browserautomation()
+	{
+		System.out.println("browserautomation");
+	}
+	
+	@Test
+	public void elementautomation()
+	{
+		System.out.println("elementautomation");
+	}
+
+}
